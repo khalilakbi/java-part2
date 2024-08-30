4ac8b730b1307000b3e6ef8dd901c5aecf2c0af8
khalil <khalil.akbi@gmail.com>
2024-08-29 21:09:04 -0400

    string concatenation

--- src/learn_string/StringDeclaration.java
+++ src/learn_string/StringDeclaration.java
@@ -2,6 +2,23 @@ package learn_string;
 
 public class StringDeclaration {
     public static void main(String[] args) {
+        // literal
         String str = "hello";
+
+        String str2 = "student";
+        //don't use this
+//        String str1 = new String("hello");
+
+        // return
+        int strLength = str.length();
+        System.out.println(strLength);
+        // concatenation
+
+        String str3 = str + " " + str2;
+        System.out.println(str3);
+        String str4 = str.concat(" "+str2);
+        System.out.println(str4);
+
+
     }
 }
